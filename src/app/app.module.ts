aa955c65c96d0d8dd7b53c0ea88929079b8e8073
Kyler Johnson <kylerjohnsondev@gmail.com>
2021-03-27 08:30:02 -0400

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersPageModule } from './users/users-page/users-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { UsersPageModule } from './users/users-page/users-page.component';
     AppRoutingModule,
     HttpClientModule,
     UsersPageModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
