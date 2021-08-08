062635257b6274fb1d0459dae2f3d367777f9f74
Kyler Johnson <kylerjohnsondev@gmail.com>
2021-08-08 17:09:01 -0400

    add new permissions service

---
+++ src/app/global/services/permissions.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Permissions } from '../models/permissions';
+
+const initialPermissionsState: Permissions = {
+  canEditPersonnel: false,
+  canDeletePersonnel: false
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionsService {
+
+  private permissionsSubject$ = new BehaviorSubject<Permissions>(initialPermissionsState);
+  permissions$: Observable<Permissions> = this.permissionsSubject$.asObservable();
+
+  updatePermission(update: Permissions | Partial<Permissions>): void {
+    const currentPermissions = this.permissionsSubject$.value;
+    const updatedPermissions = { ...currentPermissions, update };
+    this.permissionsSubject$.next(updatedPermissions);
+  }
+}
